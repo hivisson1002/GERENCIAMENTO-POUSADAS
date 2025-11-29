f78969f461d075590a005a378fcc815a02400177
Hebert_Ivisson <hias1@aluno.ifal.edu.br>
2025-11-29 11:06:10 -0300

    feat: configurar PostgreSQL com Docker

---
+++ src/conexão/Conexao.java
@@ -0,0 +1,21 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    public static Connection getConexao() {
+        try {
+            Connection conn = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/pousadas",
+                "postgres",
+                "1234"
+            );
+            return conn;
+        } catch (SQLException e) {
+            System.out.println("Erro ao se conectar com o banco: " + e.getMessage());
+            return null;
+        }
+    }
+}
